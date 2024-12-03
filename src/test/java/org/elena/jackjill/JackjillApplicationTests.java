721418dd4394aed3c88845bc079fa44bc2f5bf60
Elena Vasilyeva <elena.vasilyeva@piano.io>
2024-12-03 18:00:59 +0400

    Init project

---
+++ src/test/java/org/elena/jackjill/JackjillApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elena.jackjill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackjillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
