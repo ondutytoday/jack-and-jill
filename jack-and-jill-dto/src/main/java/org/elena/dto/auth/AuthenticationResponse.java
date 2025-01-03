870c60d6f93baecf77830ff0928c1c81137f709a
Elena Vasilyeva <elena.vasilyeva@piano.io>
2025-01-03 21:06:19 +0400

    Add swagger description to dto

--- jack-and-jill-dto/src/main/java/org/elena/dto/auth/AuthenticationResponse.java
+++ jack-and-jill-dto/src/main/java/org/elena/dto/auth/AuthenticationResponse.java
@@ -2,7 +2,12 @@ package org.elena.dto.auth;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record AuthenticationResponse(
-        @JsonProperty("access_token") String accessToken
+
+        @Schema(description = "Bearer token")
+        @JsonProperty("access_token")
+        String accessToken
 ) {
 }
--- jack-and-jill-dto/src/main/java/org/elena/dto/auth/ChangePasswordRequest.java
+++ jack-and-jill-dto/src/main/java/org/elena/dto/auth/ChangePasswordRequest.java
@@ -2,12 +2,33 @@ package org.elena.dto.auth;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public record ChangePasswordRequest(
-        @NotEmpty @Size(max = 200) @JsonProperty("username") String username,
-        @NotEmpty @Size(max = 200) @JsonProperty("current_password") String currentPassword,
-        @NotEmpty @Size(max = 200) @JsonProperty("new_password") String newPassword,
-        @NotEmpty @Size(max = 200) @JsonProperty("confirm_password") String confirmPassword) {
+
+        @Schema(description = "Username of the user", example = "john_doe", maxLength = 200, minLength = 1)
+        @NotEmpty
+        @Size(max = 200)
+        @JsonProperty("username")
+        String username,
+
+        @Schema(description = "Current password", maxLength = 200, minLength = 1)
+        @NotEmpty
+        @Size(max = 200)
+        @JsonProperty("current_password")
+        String currentPassword,
+
+        @Schema(description = "New password", maxLength = 200, minLength = 1)
+        @NotEmpty
+        @Size(max = 200)
+        @JsonProperty("new_password")
+        String newPassword,
+
+        @Schema(description = "New password confirmation", maxLength = 200, minLength = 1)
+        @NotEmpty
+        @Size(max = 200)
+        @JsonProperty("confirm_password")
+        String confirmPassword) {
 }
